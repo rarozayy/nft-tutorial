81f248d8289a4e6f8719e19a4eee60260ee6acf1
Rayan Zahem <rayanzahem@gmail.com>
2022-03-06 18:07:39 +0100

    .

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,19 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+* @type import('hardhat/config').HardhatUserConfig
+*/
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("./scripts/deploy.js");
+require("./scripts/mint.js");
+require("@nomiclabs/hardhat-etherscan");
 
-const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
-  solidity: "0.8.0",
-  defaultNewtwork: "rinkeby",
-  networks: {
+   solidity: "0.8.0",
+   defaultNetwork: "rinkeby",
+   networks: {
     hardhat: {},
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
@@ -22,5 +25,7 @@ module.exports = {
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
     },
   },
-
+  etherscan: {
+    apiKey:ETHERSCAN_API_KEY,
+  },
 }
